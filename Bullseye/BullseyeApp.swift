c6f3daaab275efa4b1d70b05e4eaa0cc79bc87f3
Jan Armbrust <jan.armbrust@googlemail.com>
2023-08-17 13:16:38 +0200

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jan Armbrust on 17.08.23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
