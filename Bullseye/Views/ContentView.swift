e103574f19a210366075ce54055b210f1216450d
Jan Armbrust <jan.armbrust@googlemail.com>
2023-09-07 14:43:47 +0200

    refine UI

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -13,43 +13,60 @@ struct ContentView: View {
     @State private var game = Game()
     
     var body: some View {
-        VStack {
-            Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
-                .bold()
-                .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
-                .font(.footnote)
-                .kerning(2.0)
-            Text(String(game.target))
-                .fontWeight(.black)
-                .font(.largeTitle)
-                .kerning(-1.0)
-            HStack {
-                Text("0")
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            VStack {
+                Text("🎯🎯🎯\nPut the Bullseye as close as you can to".uppercased())
                     .bold()
-                Slider(value: $sliderValue, in: 1.0...100.0)
-                Text("100")
-                    .bold()
-            }
-            Button("Hit me") {
-                alertIsVisible = true
-            }
-            .alert(
-                "Hello there!",
-                isPresented: $alertIsVisible,
-                actions: {
-                    Button("Hit me") {
-                        print("Alert closed")
-                    }
-                },
-                message: {
-                    let roundedValue = Int(sliderValue.rounded())
-                    Text("""
-                        The slider's value is \(roundedValue).
-                        You scored \(game.points(sliderValue: roundedValue)) this round
-                    """)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4.0)
+                    .font(.footnote)
+                    .kerning(2.0)
+                    .padding(.horizontal, 30)
+                Text(String(game.target))
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .kerning(-1.0)
+                HStack {
+                    Text("0")
+                        .bold()
+                    Slider(value: $sliderValue, in: 1.0...100.0)
+                    Text("100")
+                        .bold()
                 }
-            )
+                .padding()
+                Button("Hit me".uppercased()) {
+                    alertIsVisible = true
+                }
+                .padding(20.0)
+                .background(
+                    ZStack {
+                        Color("ButtonColor")
+                        LinearGradient(colors: [Color.white.opacity(0.3), Color.clear], startPoint: .top, endPoint: .bottom)
+                    }
+                )
+                .foregroundColor(.white)
+                .cornerRadius(21.0)
+                .bold()
+                .font(.title3)
+                .alert(
+                    "Hello there!",
+                    isPresented: $alertIsVisible,
+                    actions: {
+                        Button("Hit me") {
+                            print("Alert closed")
+                        }
+                    },
+                    message: {
+                        let roundedValue = Int(sliderValue.rounded())
+                        Text("""
+                            The slider's value is \(roundedValue).
+                            You scored \(game.points(sliderValue: roundedValue)) this round
+                        """)
+                    }
+                )
+            }
         }
     }
 }
